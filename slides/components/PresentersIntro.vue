9110f72f7ccc8284b34f6e3c72ef6458990f1898
Tien-Cheng <tiencheng@protonmail.com>
2026-05-27 19:20:49 +0800

    feat(slides): make the presenters slide look better

---
+++ slides/components/PresentersIntro.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const tienCheng = {
+	index: "01",
+	name: "Tien Cheng",
+	photo: "/presenters/tiencheng.jpg",
+	program: "Y1 CS",
+	affiliationUrl: "https://www.nushackers.org/",
+	photoPosition: "center 28%",
+};
+
+const putt = {
+	index: "02",
+	name: "Putt",
+	photo: "/presenters/putt.jpg",
+	program: "Y1 BAIS",
+	affiliationUrl: "https://www.nushackers.org/",
+	photoPosition: "center 32%",
+};
+</script>
+
+<template>
+  <div class="presenters-intro">
+    <div class="nus-terminal-eyebrow presenters-eyebrow">
+      <span>~</span><span>/</span>presenters
+    </div>
+
+    <PresenterCard v-bind="tienCheng" />
+    <PresenterCard v-bind="putt" />
+  </div>
+</template>
